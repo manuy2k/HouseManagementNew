3b2bec1483892214ab6ba327611da8746b987f7f
manuy2k <47465951+manuy2k@users.noreply.github.com>
2020-07-07 20:23:29 +0530

    At this point...
    Bootstrap is working for all modules(Lazy Loaded too).
    
    No major fixes or updates.

--- src/app/HouseAdmin/components/daily-expenses/daily-expenses.component.ts
+++ src/app/HouseAdmin/components/daily-expenses/daily-expenses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { exit } from 'process';
 
 @Component({
   selector: 'app-daily-expenses',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DailyExpensesComponent implements OnInit {
 
+  itemsCount: number = 0;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  IncDec(val){
+    if(val==1){
+      this.itemsCount++;
+      console.log("Increment");
+    }
+      
+    if(val==0){
+      if(this.itemsCount==0)
+      return;
+      this.itemsCount--;
+      console.log("Decrement");
+    }
+      
   }
 
 }
